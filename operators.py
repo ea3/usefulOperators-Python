a30a6023f1ac7792b93193747b01a136c08f89e2
Emilio Araos <ea3dev@gmail.com>
2020-01-11 16:26:39 -0700

    exercises

--- operators.py
+++ operators.py
@@ -65,10 +65,74 @@ result = input('Enter a number here: ')
 print(type(result))
 
 
+mystring = 'hello'
+mylist = []
+for letter in mystring:
+	mylist.append(letter)
+
+print(mylist)
+
+
+
+mylist = [x for x in 'word']
+print(mylist)
+
+
+
+## LIST COMPREHENSION
+
+mylist = [x**2 for x in range(0,11) if x % 2 == 0]
+print (mylist)
+
+# Fahrenheit conversion
+
+celcius = [0,10,20,34.5]
+
+fahrenheit = [((9/5)*temp + 32) for temp in celcius]
+
+print(fahrenheit)
+
+fahrenheit = []
+for temp in celcius : 
+	fahrenheit.append(((9/5)*temp + 32) )
+print(fahrenheit)
+
+mylist = []
+for x in [2,4,6]:
+	for y in [100,200,300]:
+		mylist.append(x*y)
+
+print(mylist)
+
+st = "Print only the words that start with s in this sentence"
+stSplit = st.split()
+print(stSplit)
+
+for word in stSplit:
+	if word[0] == 's':
+		print(word)
+
+## print even number from 0 to 10 using range. 
+print(range(0,11,2))
+
+
+
+print([x for x in range(1,51) if x % 3 == 0])
 
 
 
 
+##fizz BUZZ!!
+
+for num in range(1,101):
+	if (num % 3 == 0) and (num % 5 == 0) :
+		print('FizzBuzz')
+	elif (num % 3 == 0):
+		print('Fizz')
+	elif (num % 3 == 0):
+		print('Buzz')
+	else:
+		print(num)
 
 
 
